031e10f498054d6da644e0b3791b4e2aed4cb3e2
Bárbara Correia <barbara.correia@corp.globo.com>
2018-08-10 15:04:50 -0300

    First commit with all project

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from  'react';
+import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
+const API_KEY = 'AIzaSyDAMlUwzU2AaJ6Hjl12uK2OrT_0-K_Udz8';
+
+YTSearch({key: API_KEY,term: 'surfboards'}, function(data) {
+    console.log(data);
+});
+
+
+// Create a new component. This component should produce some HTML
+class App extends Component { //Instance of App
+
+    constructor(props)
+    {
+        super(props);
+
+        this.state = { videos:[] }; // Initial State of Array of Videos
+
+        YTSearch({key: API_KEY,term: 'surfboards'}, (videos) => {
+            this.setState({ videos: videos}); // State after search videos
+        });
+
+    }
+    render() {
+        return (
+         <div>
+            <SearchBar />
+            <VideoList videos={this.state.videos} />
+        </div>
+        );
+    }
+}
+
+// Take this components generate HTML and put it on the page (in the DOM-html)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
