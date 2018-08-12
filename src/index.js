8497ffd7ca7022b9d7ced9962efc31fc1cf4c010
Bárbara Correia <barbaracorreia@MacBook-Pro-de-Barbara.local>
2018-08-11 23:27:09 -0300

    WIP

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from  'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyDAMlUwzU2AaJ6Hjl12uK2OrT_0-K_Udz8';
 
@@ -28,6 +29,7 @@ class App extends Component { //Instance of App
         return (
          <div>
             <SearchBar />
+            <VideoDetail video={this.state.videos[0]} />
             <VideoList videos={this.state.videos} />
         </div>
         );
